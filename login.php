3c7feadbb950e915db80c863a441f96f2307c8f7
ismahen123 <161716281+ismahen123@users.noreply.github.com>
2024-02-29 20:05:32 +0100

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 // Connexion à la base de données
-$servername = "127.0.0.1";
+$servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "akwel_db";
@@ -17,10 +17,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['signup'])) {
     $familyname = $_POST['familyname'];
     $poste = $_POST['poste'];
     $email = $_POST['email'];
-    $cin = $_POST['cin'];
-    $password = $_POST['password'];
+    $cin = $_POST['id'];
+    $password = $_POST['pswd'];
+    $confirmPassword = $_POST['confirm_password'];
+
+    // Validation des données
+    if (empty($username) || empty($familyname) || empty($poste) || empty($email) || empty($cin) || empty($password) || empty($confirmPassword)) {
+        echo "Tous les champs sont obligatoires.";
+        exit;
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Email invalide.";
+        exit;
+    }
+
+    if (strlen($password) < 8) {
+        echo "Le mot de passe doit comporter au moins 8 caractères.";
+        exit;
+    }
+
+    if ($password !== $confirmPassword) {
+        echo "Les mots de passe ne correspondent pas.";
+        exit;
+    }
+
+    // Vérification si l'email est déjà utilisé
+    $query = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($query);
+    if ($result->num_rows > 0) {
+        echo "Cet email est déjà utilisé.";
+        exit;
+    }
+
+    // Hashage du mot de passe
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
+    // Insertion des données dans la base de données
     $query = "INSERT INTO users (username, familyname, poste, email, CIN, password) VALUES ('$username', '$familyname', '$poste', '$email', '$cin', '$hashed_password')";
 
     if ($conn->query($query) === TRUE) {
@@ -35,6 +68,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
     $email = $_POST['email'];
     $password = $_POST['pswd'];
 
+    // Validation des données
+    if (empty($email) || empty($password)) {
+        echo "Email et mot de passe sont obligatoires.";
+        exit;
+    }
+
+    // Récupération de l'utilisateur depuis la base de données
     $query = "SELECT * FROM users WHERE email='$email'";
     $result = $conn->query($query);
 
